8936ce10bb974f3769e964d5432d76c1f9611e57
Minnie Kim <minniek@bu.edu>
2015-03-26 09:29:23 -0400

    changed json deserialization method to use HttpURLConnection instead of HttpClient for easier testing

--- Alpha2_App1/app/src/main/java/com/example/minnie/alpha2_app1/MainActivity.java
+++ Alpha2_App1/app/src/main/java/com/example/minnie/alpha2_app1/MainActivity.java
@@ -16,13 +16,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
@@ -32,33 +33,66 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
-
+public class MainActivity extends ActionBarActivity {
     // Declare widgets
     Button sendBtn, receiveBtn, clearBtn;
     TextView sendTv, receiveTv;
     Toolbar toolbar;
 
-    String responseStr = "";
-    String TAG = "Alpha2_App1";
-    Boolean isGet; // flag to determine GET or POST request execution
-
-    // Send JSON string to:
-    String urlSend = "http://www.httpbin.org/post"; // A test server that accepts post requests
-
-    // Receive JSON string from:
-    String urlReceive = "http://jsonip.com/"; // Returns public IP of device
+    boolean isSend; // flag to determine Send or Receive execution
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Buttons
-        sendBtn = (Button)findViewById(R.id.send_btn); sendBtn.setOnClickListener(this);
-        receiveBtn = (Button)findViewById(R.id.receive_btn); receiveBtn.setOnClickListener(this);
-        clearBtn = (Button)findViewById(R.id.clear_btn); clearBtn.setOnClickListener(this);
+        // Send - create POJO, serialize it to JSON Object, and send POST request
+        sendBtn = (Button)findViewById(R.id.send_btn);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isSend = true;
+                String urlSend = "http://www.httpbin.org/post"; // A test server that accepts post requests
+
+                // Check for network connection
+                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo nwInfo = connMgr.getActiveNetworkInfo();
+                // If network is present, start AsyncTask to connect to given URL
+                if ((nwInfo != null) && (nwInfo.isConnected())) {
+                    new StartAsyncTask().execute(urlSend);
+                } else {
+                    sendTv.setText("ERROR No network connection detected.");
+                }
+        }});
+
+        // Receive - get JSON string from server, deserialize it to POJO
+        receiveBtn = (Button)findViewById(R.id.receive_btn);
+        receiveBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                isSend = false;
+                // Receive JSON string from:
+                String urlReceive = "http://jsonip.com/"; // Returns public IP of device
+
+                // Check for network connection
+                ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo nwInfo = connMgr.getActiveNetworkInfo();
+                // If network is present, start AsyncTask to connect to given URL
+                if ((nwInfo != null) && (nwInfo.isConnected())) {
+                    new StartAsyncTask().execute(urlReceive);
+                } else {
+                    receiveTv.setText("ERROR No network connection detected.");
+                }
+            }
+        });
+
+        clearBtn = (Button)findViewById(R.id.clear_btn);
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                sendTv.setText("");
+                receiveTv.setText("");
+            }
+        });
 
         // TextViews
         sendTv = (TextView)findViewById(R.id.send_textView);
@@ -89,46 +123,10 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
     }
 
-    // Assign tasks for buttons
-    public void onClick(View v) {
-        if (v == sendBtn) {
-            // Set flag to execute POST request in AsyncTask
-            isGet = false;
-
-            // Check for network connection
-            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo nwInfo = connMgr.getActiveNetworkInfo();
-            // If network is present, start AsyncTask to connect to given URL
-            if ((nwInfo != null) && (nwInfo.isConnected())) {
-                new StartAsyncTask().execute(urlSend);
-            } else {
-                sendTv.setText("ERROR No network connection detected.");
-            }
-        }
-
-        if (v == receiveBtn) {
-            // Set flag to execute GET request in AsyncTask
-            isGet = true;
-
-            // Check for network connection
-            ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-            NetworkInfo nwInfo = connMgr.getActiveNetworkInfo();
-            // If network is present, start AsyncTask to connect to given URL
-            if ((nwInfo != null) && (nwInfo.isConnected())) {
-                new StartAsyncTask().execute(urlReceive);
-            } else {
-                receiveTv.setText("ERROR No network connection detected.");
-            }
-        }
-
-        if (v == clearBtn) {
-            // Clear out text views
-            sendTv.setText("");
-            receiveTv.setText("");
-        }
-    }
-
     protected class StartAsyncTask extends AsyncTask<String, Void, String> {
+        String TAG = "Alpha2_App1"; // For Log.d
+        String responseStr = "";
+
         @Override
         protected String doInBackground(String... urls) {
             // Params come from the execute() call: params[0] is the url
@@ -141,84 +139,60 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
 
         protected void onPostExecute(String responseStr) {
-            if (isGet == true) {
-                sendTv.setText("Successfully sent JSON string to " + urlSend + "\n\n" + "The following" +
+            if (isSend) {
+                sendTv.setText("Successfully sent JSON string to \"http://www.httpbin.org/post\"" + "\n\n" + "The following" +
                         " is the response from the server: " + responseStr);
-            } else if (isGet == false) {
-                Gson gson = new Gson();
-                JsonIP jsonIp = gson.fromJson(responseStr, JsonIP.class);
-                receiveTv.setText("Successfully received JSON string from " + urlReceive + "\n\n" + "The following" +
-                        " is the response from the server: " + jsonIp);
+            } else if (!isSend) {
+                receiveTv.setText("Successfully received the following JSON string from \"http://jsonip.com/\"" + "\n\n"
+                        + responseStr);
             }
         }
 
         private String connectToURL(String url) throws IOException {
-            // Execute the following if "Send" button is pressed
-            if (isGet == true) {
-                try {
-                    URL myURL = new URL(url);
-                    HttpURLConnection conn = (HttpURLConnection) myURL.openConnection();
+            URL myURL = new URL(url);
+            HttpURLConnection conn =  (HttpURLConnection) myURL.openConnection();
+            conn.setReadTimeout(10 * 1000); // milliseconds
+            conn.setConnectTimeout(10 * 1000); // milliseconds
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            conn.connect();
 
-                    conn.setReadTimeout(10 * 1000); // milliseconds
-                    conn.setConnectTimeout(10 * 1000); // milliseconds
-                    conn.setDoOutput(true);
-                    conn.setDoInput(true);
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/json");
-                    conn.setRequestProperty("Accept", "application/json");
-                    conn.connect();
+            // If Send button is pressed, create data object, convert it to JSON string, and send it
+            if (isSend) {
+                Data dataObj = new Data();
+                Gson gson = new Gson();
+                String dataJsonStr = gson.toJson(dataObj); // Serialize data object to string
+                Log.d(TAG, "dataJsonStr: " + dataJsonStr);
 
-                    // Create Data POJO and convert to JSON string
-                    Data data = new Data();
-                    Gson gson = new Gson();
-                    String dataGsonStr = gson.toJson(data);
-
-                    // Send JSON string to server
-                    DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
-                    dos.write(dataGsonStr.getBytes());
-                    int responseCode = conn.getResponseCode();
-                    Log.d(TAG, "Response code: " + responseCode);
-
-                    InputStream is = conn.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-                    StringBuilder content = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        content.append(line);
-                    }
-                    responseStr = content.toString();
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                    responseStr = "ERROR MalformedURLException caught: " + e;
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    responseStr = "ERROR IOException caught: " + e;
-                }
-            } else if (isGet == false) { // Execute the following if "Receive" button is pressed
-                HttpClient httpClient = new DefaultHttpClient();
-                HttpGet httpGet = new HttpGet(url);
-
-                HttpResponse httpResponse = httpClient.execute(httpGet);
-                int responseCode = httpResponse.getStatusLine().getStatusCode();
-                Log.d(TAG, "Response code: " + responseCode);
-                HttpEntity entity = httpResponse.getEntity();
-
-                if (entity != null) {
-                    InputStream response = entity.getContent();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(response));
-                    StringBuilder content = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        content.append(line);
-                    }
-                    responseStr = content.toString();
-                }
+                // Send JSON string to server
+                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+                dos.write(dataJsonStr.getBytes());
             }
+
+            // Retrieve response from server
+            InputStream is = conn.getInputStream();
+            Log.d(TAG, is.toString());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder content = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+            responseStr = content.toString();
+
+            // If Receive is pressed, deserialize "responseStr" into JsonIP object
+            if (!isSend) {
+                Gson gson = new Gson();
+                JsonIP jsonIp = gson.fromJson(responseStr, JsonIP.class); // Deserialize
+                Log.d(TAG, jsonIp.toString()); // Temp sink
+            }
+
             return responseStr;
         }
     }
 
-    // POJO for send
+    /**************************************** POJOs ****************************************/
+    // Object to send
     public class Data {
         private int dataInt = 700;
         private String dataStr = "EC700 Alpha2";
@@ -240,7 +214,7 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         }
     }
 
-    // POJO for receive
+    // Object to receive
     public class JsonIP {
         private String ip;
         private String about;
